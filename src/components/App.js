19e0779e20e6f70f192d9319f4476fec7fe3fdd9
Pratham700 <prathamsinghal.singhal@gmail.com>
2020-11-18 17:04:13 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,52 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props) ;
+        this.state = {
+            time: new Date(),
+        };
+     this.intervalId = null ;   
+    }
     render() {
 
         return(
             <>
-               
+               <div className = "Clock">
+                   <h3 id = "time">{this.getTimeString()}</h3>
+               </div>
             </>
-        )
+        );
+    }
+    componentDidMount() {
+        this.intervalId = setInterval(() => {
+            this.setState({
+                time:new Date() 
+            })
+        },1*1000) ;
+    }
+    componentWillUnmount() {
+        clearInterval(this.intervalId) ;
+    }
+
+    getTimeString() {
+        const currTime = this.state.time ;
+        const [hours,minutes,seconds] = [
+            currTime.getHours(),
+            currTime.getMinutes(),
+            currTime.getSeconds()
+        ] ;
+
+        const amPm = hours>= 12 ? "PM":"AM" ;
+
+        const twelveHourSys = hours>12 ? hours-12 : hours ;
+
+        const hourString = padNumberToString(twelveHourSys) ;
+        const minuteString = padNumberToString(minutes) ;
+        const secondsString = padNumberToString(seconds) ;
+
+        const timeDisplay = `${hourString}:${minuteString}:${secondsString} ${amPm}` ;
+        return timeDisplay ;
     }
 }
 
